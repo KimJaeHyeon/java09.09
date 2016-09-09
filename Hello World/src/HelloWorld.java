0b3344ee1702c1ba20d5af6b4e0ca244e7bbc0c1
soft <soft@soft-PC>
2016-09-09 11:05:01 +0900

    add print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 	public static void main (String[] aigs){
 		System.out.println("Hello World by KimJaeHyeon");
-	
+		System.out.println("World's End by KimJaeHyeon");
 	}
 	
 }
