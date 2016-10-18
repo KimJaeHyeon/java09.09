d101346982d5cb8d15f7dadd8b8ad0c7dd32a45b
KimJaeHyeon <kjh94kjh94@naver.com>
2016-10-18 16:37:30 +0900

    1018  제발12

--- 1018.practice/src/exercise.java
+++ 1018.practice/src/exercise.java
@@ -3,7 +3,7 @@ public class exercise {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("aaaaaaaaaaaaaaaaaaddddda");
+System.out.println("aaaaaa123aaaaaaaaaddddda");
 System.out.println("aaaaaaaaa1231asd125435a");
 	}
 
