216f397c14f718c67aaa6371afaa165a8bd2b870
KimJaeHyeon <kjh94kjh94@naver.com>
2016-10-18 16:46:42 +0900

    please

--- 1018.practice/src/exercise.java
+++ 1018.practice/src/exercise.java
@@ -4,7 +4,7 @@ public class exercise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("aaaaaa123aaaaaaaaaddddda");
-System.out.println("aaaaaaaaa1231asd125435a");
+System.out.println("aaaaaaaasdasdaa1231asd125435a");
 	}
 
 }
